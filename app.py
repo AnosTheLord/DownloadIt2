2f8052fcc17efac980709b1a2b8615cefac5092d
AnosTheLord <136479726+AnosTheLord@users.noreply.github.com>
2026-03-19 11:24:42 +0530

    Fix formatting issues and update download options

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def download():
 
     # 🔥 FIX: NO FFMPEG NEEDED
     ydl_opts = {
-        'format': 'best',   # ✅ single file (no merge)
+        best[ext=mp4]/best,   # ✅ single file (no merge)
         'outtmpl': output
     }
 
